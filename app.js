d0c71b31ad1fa273e630686258727ba7ff69a8de
Tayyab Zaman <tayyabzaman807@gmail.com>
2026-05-09 09:41:14 +0500

    Add CORS middleware to Express app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { config } = require("dotenv");
-
+const cors = require("cors")
 const app = express();
+
+app.use(cors())
+
 
 app.get("/", (req, res) => {
   res.send("This is Home Page");
