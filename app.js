e55719efea25150b4480904c65301f807a9d2898
Tayyabzaman1357 <tayyabzaman1357@gmail.com>
2026-05-09 09:33:35 +0500

    Express code

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { config } = require("dotenv");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("This is Home Page");
+});
+app.get("/products",(req,res) =>{
+    res.json({Products})
+})
+
+
+
+const Products = [
+  { id: 1, name: "Product 1", price: 100 },
+  { id: 2, name: "Product 2", price: 200 },
+  { id: 3, name: "Product 3", price: 300 },
+  { id: 4, name: "Product 4", price: 100 },
+  { id: 5, name: "Product 5", price: 200 },
+  { id: 6, name: "Product 6", price: 300 },
+  { id: 7, name: "Product 7", price: 100 },
+  { id: 8, name: "Product 8", price: 200 },
+  { id: 9, name: "Product 9", price: 300 },
+  { id: 10, name: "Product 10", price: 300 },
+];
+
+
+
+const Port = process.env.PORT || 4000;
+
+app.listen(Port, (req, res) => {
+  console.log("This is server running" + Port);
+});
